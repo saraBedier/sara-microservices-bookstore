772d540d0ba87bb734d882dfc628853ca528067a
Sara <Toshiba@host.docker.internal>
2025-09-18 18:01:48 +0400

    start

---
+++ book-service/book-service/src/test/java/com/sara/bookstore/book_service/BookServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.bookstore.book_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ order-service/order-service/src/test/java/com/sara/bookstore/order_service/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.bookstore.order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
